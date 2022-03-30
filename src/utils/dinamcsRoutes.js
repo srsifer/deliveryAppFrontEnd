3cc2fae3e975982a8892942d498e35ea5dc73ac5
igor <igorsife80@gmail.com>
2022-03-30 20:15:25 -0300

    corrige variavel de ambiente

--- src/utils/dinamcsRoutes.js
+++ src/utils/dinamcsRoutes.js
@@ -1,4 +1,4 @@
-const serverUrl = process.env.NODE_ENV === 'production'
+const serverUrl = process.env.REACT_APP_NODE_ENV === 'production'
   ? 'https://in-tregasbackend.herokuapp.com'
   : 'localhost:3001/'
 // const serverUrl = 'https://in-tregasbackend.herokuapp.com';
