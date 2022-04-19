0b03b0298d6c517eff7d8a07491aa2364689eb2c
igor <igorsife80@gmail.com>
2022-04-19 11:39:31 -0300

    feat: ajuste css tela de registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,8 +4,11 @@ import { registerValidation } from '../utils/inputValidations';
 import { apiRegister } from '../services/apiCalls';
 import {
   LoguinDiv,
+  LogoDoApp,
+  InputsDiv,
+  DivButonsLoguin,
   Inputs,
-  ButonsSend,
+  ButonsRegister,
 } from '../styles/login/Loguinstyles';
 
 export default function RegisterUser() {
@@ -42,47 +45,52 @@ export default function RegisterUser() {
 
   return (
     <LoguinDiv>
-
-      { redirectOn ? <Redirect to="/customer/products" /> : null }
-
-      <p
-        hidden={ hiddenOn }
-        data-testid="common_register__element-invalid_register"
-      >
-        invalid credential
-      </p>
-      <Inputs
-        name="name"
-        type="name"
-        value={ newUser.name }
-        onChange={ handleChange }
-        placeholder="Insira um nome"
-        data-testid="common_register__input-name"
-      />
-      <Inputs
-        name="email"
-        type="text"
-        value={ newUser.email }
-        onChange={ handleChange }
-        placeholder="Insira um e-email"
-        data-testid="common_register__input-email"
-      />
-      <Inputs
-        name="password"
-        type="password"
-        value={ newUser.password }
-        onChange={ handleChange }
-        placeholder="Insira uma senha"
-        data-testid="common_register__input-password"
-      />
-      <ButonsSend
-        type="submit"
-        disabled={ switchDisabledButton() }
-        onClick={ () => sendRegister() }
-        data-testid="common_register__button-register"
-      >
-        register now
-      </ButonsSend>
+      <InputsDiv>
+       <LogoDoApp/>
+        { redirectOn ? <Redirect to="/customer/products" /> : null }
+        <div>
+        <Inputs
+          name="name"
+          type="name"
+          value={ newUser.name }
+          onChange={ handleChange }
+          placeholder="Insira um nome"
+          data-testid="common_register__input-name"
+        />
+        <Inputs
+          name="email"
+          type="text"
+          value={ newUser.email }
+          onChange={ handleChange }
+          placeholder="Insira um e-email"
+          data-testid="common_register__input-email"
+        />
+        <Inputs
+          name="password"
+          type="password"
+          value={ newUser.password }
+          onChange={ handleChange }
+          placeholder="Insira uma senha"
+          data-testid="common_register__input-password"
+        />
+        <p
+          hidden={ hiddenOn }
+          data-testid="common_register__element-invalid_register"
+        >
+          invalid credential
+        </p>
+        </div>
+        <DivButonsLoguin>
+          <ButonsRegister
+            type="submit"
+            disabled={ switchDisabledButton() }
+            onClick={ () => sendRegister() }
+            data-testid="common_register__button-register"
+          >
+            Criar Conta
+          </ButonsRegister>
+        </DivButonsLoguin>
+      </InputsDiv>
     </LoguinDiv>
   );
 }
