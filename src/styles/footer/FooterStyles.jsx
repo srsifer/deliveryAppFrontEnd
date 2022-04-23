79e175a814bcb0efd2af7a873d858f7f35752c2a
igor <igorsife80@gmail.com>
2022-04-23 14:48:18 -0300

    adiciona um footer provisorio

---
+++ src/styles/footer/FooterStyles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FooterStyle = styled.footer`
+    background-color: #036FB7;
+    padding: 10px;
+    bottom: 0;
+    position:relative;
+    color: white;
+`;
