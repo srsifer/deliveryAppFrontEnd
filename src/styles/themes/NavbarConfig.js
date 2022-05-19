27bdf191de3247aa998b4caac5e301230290b8c6
igor s fernandes <igorsife80@gmail.com>
2022-05-19 18:45:07 -0300

    cria a config de rederização da navbar

---
+++ src/styles/themes/NavbarConfig.js
@@ -0,0 +1,22 @@
+export const navbarConfig = {
+    costumer : {
+        products: {
+            width: '20%',
+        },
+        
+        orders: {
+            width: '50%',
+        },
+    },
+
+    saller: {
+        orders: {
+            width: '70%',
+        },
+
+    },
+
+    manager: {
+        orders: {},
+    }
+};
