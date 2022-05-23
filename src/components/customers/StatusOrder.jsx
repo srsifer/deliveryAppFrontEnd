d7cd898a5cdb91b09333ed026f6b67e78d932063
jmlinger <jmlinger10@gmail.com>
2022-05-23 20:36:42 -0300

    fix(customers/StatusOrder): altera desestruturação de props e corrige proptypes.

--- src/components/customers/StatusOrder.jsx
+++ src/components/customers/StatusOrder.jsx
@@ -2,28 +2,30 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { StatusOrderStyles } from '../../styles/ordersStyles/orderStyles'
 
-export default function StatusOrder({ order: { status, id } }) {
-    const [theme, setTheme] = useState('#d3c63c');
+export default function StatusOrder(props) {
+  const { order: { status, id } } = props;
+  const [theme, setTheme] = useState('#d3c63c');
 
-    useEffect(() => {
-      const selectBackground = () => {
-        if (status === 'Preparando') (setTheme('#87d53c'));
-        if (status === 'Entregue') (setTheme('#3bd5b0'));
-        if (status === 'Em Trânsito') (setTheme('#E8520D'));
-      };
-      selectBackground();
-    }, [status]);
+  useEffect(() => {
+    const selectBackground = () => {
+      if (status === 'Preparando') (setTheme('#87d53c'));
+      if (status === 'Entregue') (setTheme('#3bd5b0'));
+      if (status === 'Em Trânsito') (setTheme('#E8520D'));
+    };
+    selectBackground();
+  }, [status]);
 
-    return (
-        <StatusOrderStyles
-          status={ theme }
-          data-testid={`customer_orders__element-delivery-status-${id}`}
-        >
-          {status}
-        </StatusOrderStyles>
-    )
+  return (
+    <StatusOrderStyles
+      status={ theme }
+      data-testid={`customer_orders__element-delivery-status-${id}`}
+    >
+      {status}
+    </StatusOrderStyles>
+  )
 }
 
-StatusOrder.propTypes = {
-  order: PropTypes.string.isRequired,
-};
+StatusOrder.propTypes = ({
+  status: PropTypes.string,
+  id: PropTypes.string,
+}).isRequired;
