f1cc775163a099979b3119e1fd1b6e480979ffbb
igor s fernandes <igorsife80@gmail.com>
2022-05-24 15:49:48 -0300

    feat: adiciona os dois arquivos que contem os nosso themas da aplicação

---
+++ src/styles/themes/light.js
@@ -0,0 +1,35 @@
+export default  {
+    title: 'light',
+
+    colors: {
+        primary: "#f6943c",
+        secundary: "#001427",
+        background: '#f5f5f5',
+        shadows: '#B2B8BC',
+    },
+
+    cardScheme: {
+        cardBg: 'white',
+        cardFooterBg: '#f9f9fa',
+        cardButtonColor: '#001427',
+        cardInputColor: 'white'
+    },
+
+    font: {
+        color: {
+            primary: '#333',
+            secundary: 'white',
+            otherColor: '#333'
+        }
+    },
+
+    totalPriceScheme: { 
+        colorBg: '#FF7F50',
+        fontColor: 'white'
+    }
+}
+
+/* "Davys Grey":"565554",
+"Blue NCS":"2e86ab",
+"Green Yellow Crayola":"f6f5ae", */
+
