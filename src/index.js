5139efee50d27aa2a45e8667d458147224854628
igor s fernandes <igorsife80@gmail.com>
2022-05-24 15:48:58 -0300

    feat: adiciona estilo clobal com crateGlobalstyle da lib styled components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { Provider } from 'react-redux'
